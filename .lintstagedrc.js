584cb91b00f1d058c606a6a6f11a89c33208b006
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-23 11:39:43 -0500

    fix lint stage

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,9 +3,8 @@ const path = require('path');
 const buildEslintCommand = filenames =>
   `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
 
-const buildPrettierCommand = filenames =>
-  `yarn prettier --write --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
+const buildPrettierCommand = filenames => `prettier --write ${filenames.join(' ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand]
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand]
 };
