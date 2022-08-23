bbed93414678a99e00e4e93cb575940814f34a46
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-08-23 11:48:51 -0500

    fix in the app

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,8 @@ const path = require('path');
 const buildEslintCommand = filenames =>
   `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`;
 
-const buildPrettierCommand = filenames => `prettier --write ${filenames.join(' ')}`;
+const buildPrettierCommand = filenames =>
+  `prettier --write ${filenames.map(f => path.relative(process.cwd(), f)).join(' ')}`;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand]
