f152aaf9852b05259379b64014ff94b9781bcb4b
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-10-19 12:53:47 -0500

    add summary

--- components/InvoicePage/InvoiceFormItemList.tsx
+++ components/InvoicePage/InvoiceFormItemList.tsx
@@ -1,43 +1,66 @@
 import React from 'react';
 import { ScrollArea } from '@mantine/core';
-import { INewInvoiceItem } from 'types';
+import { IInvoiceSummary, INewInvoiceItem } from 'types';
 import InvoiceFormItemListItem from './InvoiceFormItemListItem';
+import { currencyFormat } from 'utils';
 
 interface Props {
   items: INewInvoiceItem[];
   removeItem(itemId: string): void;
+  summary: IInvoiceSummary;
 }
 
-const InvoiceFormItemList = ({ items, removeItem }: Props) => {
+const InvoiceFormItemList = ({ items, removeItem, summary }: Props) => {
   return (
-    <ScrollArea className="relative h-40 overflow-y-auto bg-gradient-to-br from-dark via-neutral-500 to-header">
-      <table className="w-full table-auto">
-        <thead className="sticky top-0 z-fixed bg-gray-dark">
-          <tr className="whitespace-nowrap text-gray-100">
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Cant.
-            </th>
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Descripción
-            </th>
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Vlr. Unt
-            </th>
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Importe
-            </th>
-            <th scope="col" className="relative px-2 py-1 text-center text-xs uppercase tracking-wide">
-              <span className="sr-only">Actions</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {items.map(item => (
-            <InvoiceFormItemListItem key={item.id} item={item} onRemove={removeItem} />
-          ))}
-        </tbody>
-      </table>
-    </ScrollArea>
+    <div>
+      <ScrollArea className="relative mb-2 h-40 overflow-y-auto bg-gradient-to-br from-dark via-neutral-500 to-header">
+        <table className="w-full table-auto">
+          <thead className="sticky top-0 z-fixed bg-gray-dark">
+            <tr className="whitespace-nowrap text-gray-100">
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Cant.
+              </th>
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Descripción
+              </th>
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Vlr. Unt
+              </th>
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Importe
+              </th>
+              <th scope="col" className="relative px-2 py-1 text-center text-xs uppercase tracking-wide">
+                <span className="sr-only">Actions</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {items.map(item => (
+              <InvoiceFormItemListItem key={item.id} item={item} onRemove={removeItem} />
+            ))}
+          </tbody>
+        </table>
+      </ScrollArea>
+      {/* SUMMARY */}
+      <div className="flex gap-x-4">
+        {/* PROPERTIES */}
+        <div className="flex-grow">
+          <div className="flex flex-col gap-y-1 text-right">
+            <p>Subtotal</p>
+            {summary.discount && <p className="text-sm">Descuento</p>}
+            <p className="text-xl">Total</p>
+          </div>
+        </div>
+        {/* VALUES */}
+        <div className="flex-shrink-0">
+          <div className="flex flex-col gap-y-1 text-right tracking-widest">
+            <p>{currencyFormat(summary.subtotal)}</p>
+            {summary.discount && <p className="text-sm">{currencyFormat(summary.discount)}</p>}
+            <p className="text-xl font-bold">{currencyFormat(summary.amount)}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- components/InvoicePage/InvoiceFormPaymentList.tsx
+++ components/InvoicePage/InvoiceFormPaymentList.tsx
@@ -1,37 +1,63 @@
 import React from 'react';
 import { ScrollArea } from '@mantine/core';
-import { INewInvoicePayment } from 'types';
+import { IInvoiceSummary, INewInvoicePayment } from 'types';
 import InvoiceFormPaymentListItem from './InvoiceFormPaymentListItem';
+import { currencyFormat } from 'utils';
 
 interface Props {
   payments: INewInvoicePayment[];
   removePayment(paymentId: number): void;
+  summary: IInvoiceSummary;
 }
 
-const InvoiceFormPaymentList = ({ payments, removePayment }: Props) => {
+const InvoiceFormPaymentList = ({ payments, removePayment, summary }: Props) => {
   return (
-    <ScrollArea className="relative h-40 overflow-y-auto bg-gradient-to-br from-dark via-neutral-500 to-header">
-      <table className="w-full table-auto">
-        <thead className="sticky top-0 z-fixed bg-gray-dark">
-          <tr className="whitespace-nowrap text-gray-100">
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Descripción
-            </th>
-            <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
-              Importe
-            </th>
-            <th scope="col" className="relative px-2 py-1 text-center text-xs uppercase tracking-wide">
-              <span className="sr-only">Actions</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {payments.map(item => (
-            <InvoiceFormPaymentListItem key={item.id} payment={item} onRemove={removePayment} />
-          ))}
-        </tbody>
-      </table>
-    </ScrollArea>
+    <div>
+      <ScrollArea className="relative mb-2 h-40 overflow-y-auto bg-gradient-to-br from-dark via-neutral-500 to-header">
+        <table className="w-full table-auto">
+          <thead className="sticky top-0 z-fixed bg-gray-dark">
+            <tr className="whitespace-nowrap text-gray-100">
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Descripción
+              </th>
+              <th scope="col" className="px-2 py-1 text-center text-xs uppercase tracking-wide">
+                Importe
+              </th>
+              <th scope="col" className="relative px-2 py-1 text-center text-xs uppercase tracking-wide">
+                <span className="sr-only">Actions</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {payments.map(item => (
+              <InvoiceFormPaymentListItem key={item.id} payment={item} onRemove={removePayment} />
+            ))}
+          </tbody>
+        </table>
+      </ScrollArea>
+
+      {/* Summary */}
+      <div className="flex flex-col gap-y-2">
+        {summary.cash && (
+          <div className="flex items-center justify-end gap-x-4">
+            <p className="tracking-wider">Efectivo: </p>
+            <p className="w-32 whitespace-nowrap text-right tracking-widest">{currencyFormat(summary.cash)}</p>
+          </div>
+        )}
+        {summary.balance && (
+          <div className="flex items-center justify-end gap-x-4">
+            <p className="tracking-wider">Crédito: </p>
+            <p className="w-32 whitespace-nowrap text-right tracking-widest">{currencyFormat(summary.balance)}</p>
+          </div>
+        )}
+        {summary.cashChange && (
+          <div className="flex items-center justify-end gap-x-4">
+            <p className="tracking-wider">Cambio: </p>
+            <p className="w-32 whitespace-nowrap text-right tracking-widest">{currencyFormat(summary.cashChange)}</p>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
