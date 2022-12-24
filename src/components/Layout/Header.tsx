90df512678e91f615aa9246de670f538c52ae111
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-12-24 11:26:01 -0500

    perf: remove the sidebar and add header nav

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,12 @@
 import { useMediaQuery } from '@mantine/hooks';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import BrandLogo from './BrandLogo';
 import BurgerToggle from './BurgerToggle';
 import DarkButtom from './DarkButton';
 import NavDrawer from './NavDrawer';
+import { Links } from './NavLinks';
 import UserAvatar from './UserAvatar';
 
 interface Props {
@@ -13,12 +16,23 @@ interface Props {
 export default function Header({ title }: Props) {
   const [drawerOpened, setDrawerOpened] = useState(false);
   const largeScreen = useMediaQuery('(min-width: 1280px)');
+  const router = useRouter();
+  const [links, setLinks] = useState(Links);
 
   const toggle = () => setDrawerOpened(current => !current);
 
   useEffect(() => {
     if (largeScreen) setDrawerOpened(false);
   }, [largeScreen]);
+
+  useEffect(() => {
+    setLinks(current =>
+      current.map(item => {
+        item.active = item.path === router.asPath;
+        return item;
+      })
+    );
+  }, [router.asPath]);
 
   return (
     <>
@@ -31,9 +45,36 @@ export default function Header({ title }: Props) {
             <BurgerToggle opened={drawerOpened} onClick={toggle} />
             <BrandLogo />
           </div>
-          <h2 className="text-base font-bold tracking-wider sm:text-xl md:text-2xl">
+          <h2 className="text-base font-bold tracking-wider sm:text-xl md:text-2xl lg:hidden">
             {title}
           </h2>
+          <nav className="hidden flex-grow px-8 lg:block">
+            <ul className="flex gap-x-2">
+              {links.map(link => (
+                <li
+                  key={link.id}
+                  className={
+                    link.active
+                      ? 'text-blue-600 dark:text-yellow-400'
+                      : 'text-dark dark:text-light' + ' transition-colors'
+                  }
+                >
+                  <Link
+                    href={link.path}
+                    passHref
+                    className="flex items-center gap-x-2 px-6 py-4 text-2xl transition-colors duration-200 hover:text-purple-600 dark:hover:text-amber-500 xl:py-3 xl:text-base"
+                  >
+                    <link.Icon
+                      size={24}
+                      stroke={1.5}
+                      className="flex-shrink-0"
+                    />
+                    <span className="font-bold tracking-wide">{link.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
           <div className="flex items-center gap-x-4">
             <DarkButtom />
             <UserAvatar />
