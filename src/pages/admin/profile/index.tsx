fd7e540d851d389f6a1b11b6473da9b0aab629fc
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-03-17 11:07:45 -0500

    feat: add form section component
    and update profile password form with the new component

--- src/pages/admin/profile/index.tsx
+++ src/pages/admin/profile/index.tsx
@@ -9,6 +9,8 @@ import { toast } from 'react-toastify';
 import { useAppDispatch } from 'src/store/hooks';
 import { logout } from 'src/features/Auth';
 import { IValidationErrors } from 'src/types';
+import FormSection from 'src/components/FormSection';
+import FormSectionCard from 'src/components/FormSectionCard';
 
 const ProfilePage: NextPage = () => {
   const [password, setPassword] = useState('');
@@ -58,91 +60,81 @@ const ProfilePage: NextPage = () => {
 
   return (
     <Layout title="Perfil de Usuario">
-      <div className="mx-auto max-w-7xl py-10 sm:px-6 lg:px-8">
-        <div className="md:grid md:grid-cols-3 md:gap-6">
-          {/* INFO */}
-          <div className="flex justify-between md:col-span-1">
-            <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium">Actualizar Contraseña</h3>
-              <p className="mt-1 text-sm text-gray-400">
-                Aseguresé de que su cuenta esté usando una contraseña larga y
-                aleatoria para mantenerse seguro.
-              </p>
+      <FormSection
+        title="Actualizar Contraseña"
+        description="Aseguresé de que su cuenta esté usando una contraseña larga y aleatoria para mantenerse seguro."
+      >
+        <FormSectionCard onSubmit={onSubmit}>
+          <FormSectionCard.Body>
+            <div className="grid grid-cols-6">
+              <PasswordInput
+                icon={<IconLock size={16} />}
+                label="Contraseña actual"
+                required
+                className="col-span-6 md:col-span-4"
+                visibilityToggleIcon={({ reveal, size }) =>
+                  reveal ? (
+                    <IconEyeOff size={size} />
+                  ) : (
+                    <IconEyeCheck size={size} />
+                  )
+                }
+                value={password}
+                onChange={({ currentTarget }) =>
+                  setPassword(currentTarget.value)
+                }
+                disabled={loading}
+                autoComplete="current-password"
+              />
+              <PasswordInput
+                icon={<IconLock size={16} />}
+                label="Nueva contraseña"
+                required
+                className="col-span-6 md:col-span-4"
+                visibilityToggleIcon={({ reveal, size }) =>
+                  reveal ? (
+                    <IconEyeOff size={size} />
+                  ) : (
+                    <IconEyeCheck size={size} />
+                  )
+                }
+                value={newPassword}
+                onChange={({ currentTarget }) =>
+                  setNewPassword(currentTarget.value)
+                }
+                disabled={loading}
+                error={errors?.password?.message}
+                autoComplete="new-password"
+              />
+              <PasswordInput
+                icon={<IconLock size={16} />}
+                label="Confirmar contraseña"
+                required
+                className="col-span-6 md:col-span-4"
+                visibilityToggleIcon={({ reveal, size }) =>
+                  reveal ? (
+                    <IconEyeOff size={size} />
+                  ) : (
+                    <IconEyeCheck size={size} />
+                  )
+                }
+                value={confirmPassword}
+                onChange={({ currentTarget }) =>
+                  setConfirmPassword(currentTarget.value)
+                }
+                disabled={loading}
+                error={errors?.confirmPassword?.message}
+                autoComplete="new-password"
+              />
             </div>
-          </div>
-          {/* FORM */}
-          <form className="mt-5 md:col-span-2 md:mt-0" onSubmit={onSubmit}>
-            <div className="bg-header px-4 py-5 shadow shadow-header sm:rounded-tl-md sm:rounded-tr-md">
-              <div className="grid grid-cols-6">
-                <PasswordInput
-                  icon={<IconLock size={16} />}
-                  label="Contraseña actual"
-                  required
-                  className="col-span-6 md:col-span-4"
-                  visibilityToggleIcon={({ reveal, size }) =>
-                    reveal ? (
-                      <IconEyeOff size={size} />
-                    ) : (
-                      <IconEyeCheck size={size} />
-                    )
-                  }
-                  value={password}
-                  onChange={({ currentTarget }) =>
-                    setPassword(currentTarget.value)
-                  }
-                  disabled={loading}
-                  autoComplete="current-password"
-                />
-                <PasswordInput
-                  icon={<IconLock size={16} />}
-                  label="Nueva contraseña"
-                  required
-                  className="col-span-6 md:col-span-4"
-                  visibilityToggleIcon={({ reveal, size }) =>
-                    reveal ? (
-                      <IconEyeOff size={size} />
-                    ) : (
-                      <IconEyeCheck size={size} />
-                    )
-                  }
-                  value={newPassword}
-                  onChange={({ currentTarget }) =>
-                    setNewPassword(currentTarget.value)
-                  }
-                  disabled={loading}
-                  error={errors?.password?.message}
-                  autoComplete="new-password"
-                />
-                <PasswordInput
-                  icon={<IconLock size={16} />}
-                  label="Confirmar contraseña"
-                  required
-                  className="col-span-6 md:col-span-4"
-                  visibilityToggleIcon={({ reveal, size }) =>
-                    reveal ? (
-                      <IconEyeOff size={size} />
-                    ) : (
-                      <IconEyeCheck size={size} />
-                    )
-                  }
-                  value={confirmPassword}
-                  onChange={({ currentTarget }) =>
-                    setConfirmPassword(currentTarget.value)
-                  }
-                  disabled={loading}
-                  error={errors?.confirmPassword?.message}
-                  autoComplete="new-password"
-                />
-              </div>
-            </div>
-            <footer className="sm: flex items-center justify-end rounded-br-md bg-gray-dark px-4 py-3 text-right shadow shadow-gray-800 sm:rounded-bl-md sm:px-6">
-              <Button type="submit" loading={loading}>
-                Guardar
-              </Button>
-            </footer>
-          </form>
-        </div>
-      </div>
+          </FormSectionCard.Body>
+          <FormSectionCard.Footer>
+            <Button type="submit" loading={loading}>
+              Guardar
+            </Button>
+          </FormSectionCard.Footer>
+        </FormSectionCard>
+      </FormSection>
     </Layout>
   );
 };
