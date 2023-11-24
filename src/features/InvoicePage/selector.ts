13898ed3c0729ede51bdd1e7b7c7bc3e5e577796
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-11-24 14:37:35 -0500

    chore: remove unused code

--- src/features/InvoicePage/selector.ts
+++
@@ -1,6 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from '@/store';
-
-export const invoicePage = (state: RootState) => state.invoicePage;
-
-export const invoicePageSelector = createSelector(invoicePage, state => state);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,6 @@ import queryClient from '@/config/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { Provider } from 'react-redux';
-import { store } from '@/store';
-import { createWrapper } from 'next-redux-wrapper';
-
 import { rootConfig } from '@/config/root-config';
 import ThemeProvider from '@/context/CustomThemeProvider';
 import { emCache } from '@/utils/emotionCache';
@@ -18,21 +14,17 @@ import { inter, poppins } from '@/config/fonts.config';
 
 rootConfig();
 
-export function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   emCache();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ThemeProvider>
-          <div className={`${inter.variable} ${poppins.variable} font-sans ${inter.className}`}>
-            <Component {...pageProps} />
-          </div>
-        </ThemeProvider>
-      </Provider>
+      <ThemeProvider>
+        <div className={`${inter.variable} ${poppins.variable} font-sans ${inter.className}`}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
-
-export default createWrapper(() => store).withRedux(MyApp);
