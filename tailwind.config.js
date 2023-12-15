a77e70dcd22b74699505a70635efdbe29ab1f337
Andres Zuñiga <andres.zuniga.063@gmail.com>
2023-12-15 15:59:57 -0500

    chore: update ESLint configuration and package dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
